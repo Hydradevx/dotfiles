c8999af21706b58f5c74c618d1a1d9b128cdc4ab
Hydradevx <themaskedprogrammer1234@gmail.com>
2025-09-05 14:20:02 +0530

    Add matugen to hyprland

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -8,15 +8,11 @@ if ! pgrep -x "swww-daemon" > /dev/null; then
     sleep 1
 fi
 
-# Pick a random wallpaper (jpg/png/webp)
 RANDOM_WALL=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) | shuf -n1)
 
 if [ -n "$RANDOM_WALL" ]; then
-    # Set wallpaper with swww
-    swww img "$RANDOM_WALL" --transition-type grow --transition-pos 0.854,0.977 --transition-step 255 --transition-fps 60
-    
     # Generate colors with Matugen
-    matugen image "$RANDOM_WALL" --apply gtk waybar
+    matugen image "$RANDOM_WALL"
     
     # Reload waybar (so new colors apply)
     pkill -SIGUSR2 waybar
