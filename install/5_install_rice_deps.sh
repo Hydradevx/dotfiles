8535b6918c5902e2f9dea527962a26502b2da4dd
Hydradevx <themaskedprogrammer1234@gmail.com>
2025-09-13 20:43:58 +0530

    add sddm

--- install/5_install_rice_deps.sh
+++ install/5_install_rice_deps.sh
@@ -18,5 +18,6 @@ run_cmd "gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'"
 run_cmd "gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'"
 
 run_cmd "cd ~ && git clone --depth 1 https://github.com/VandalByte/darkmatter-grub2-theme.git && cd darkmatter-grub2-theme && sudo python3 darkmatter-theme.py --install"
+run_cmd "sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)""
 
 log_success "All rice dependencies installed"
